4d3e935f801ed657c1a39c39addea7b28ac9d67a
JustFollowMe <1930057443@qq.com>
2017-05-04 17:41:29 +0800

    First Commit...

---
+++ WebContent/web/css/index.js
@@ -0,0 +1,17 @@
+﻿
+
+function pageWidth(){
+    var ww = document.body.scrollWidth;
+    if(ww>580)
+    {
+        var bgs = document.getElementById("bgs");
+        bgs.style.width = "480px";
+        bgs.style.marginLeft = "auto";
+        bgs.style.marginRight = "auto";
+        
+    }
+}
+window.onload = pageWidth;
+
+
+
